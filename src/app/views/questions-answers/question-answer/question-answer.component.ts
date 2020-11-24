9c88b6ee2a886ac6d037f02c283f27f90e19db53
lopes-leandro <lopesleandro.it@gmail.com>
2020-11-23 22:54:45 -0300

    Implementado o Question Card
    1. Implementada as funcionalidade:
    	Editar
    	Exclur
    	Checked Correct
    	Checked Incorrect
    2. Implementado a funcionalidade de toggle para mostrar os detalhes do card.
    3. Ao marcar o card como Correct, todo o header fica na cor verde.
    4. Ao marcar o card como Incorrect, todo o header fica na cor vermelha.

--- src/app/views/questions-answers/question-answer/question-answer.component.ts
+++ src/app/views/questions-answers/question-answer/question-answer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IQuestion } from 'src/app/models/question';
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10000);
+}
 
 @Component({
   selector: 'app-question-answer',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionAnswerComponent implements OnInit {
 
+  flashs: IQuestion[] = [{
+    question: 'Question 1',
+    answer: 'Answer 1',
+    show: false,
+    id: getRandomNumber(),
+  },{
+    question: 'Question 2',
+    answer: 'Answer 2',
+    show: false,
+    id: getRandomNumber(),
+  },{
+    question: 'Question 3',
+    answer: 'Answer 3',
+    show: false,
+    id: getRandomNumber(),
+  }];
+
+  editing = false;
+  editingId: number;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  trackByFlashId(index, flash){
+    return flash.id
+  }
+
+  handleToggleCard(id: number) {
+    const flash = this.flashs.find(flash => flash.id === id);
+    flash.show = !flash.show
+  }
+
+  handleDelete(id: number) {
+    const flashId = this.flashs.findIndex(flash => flash.id === id);   
+    this.flashs.splice(flashId, 1);
+  }
+
+  handleEdit(id: number) {
+    this.editing = true;
+    this.editingId = id;
+    // TODO: Adicionaremos lógica de edição após adicionar o formulário
+  }
+
+  handleRememberedChange({id, flag}) {
+    const flash = this.flashs.find(flash => flash.id === id);
+    flash.remembered = flag;
+  }
 }
