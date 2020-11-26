6da4b486ccc06bd445b71299b40499ca50dcb8ca
Leandro Lopes <lopesleandro.it@gmail.com>
2020-11-26 14:37:13 -0300

    Implement Template Form
    1. Implemented fields with Angular Material Input
    2. Implemented two-way data binding in fields
    3. Implemented ngModel

--- src/app/@custom-material/custom-material.module.ts
+++ src/app/@custom-material/custom-material.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatCardModule} from '@angular/material/card'; 
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -26,8 +25,6 @@ import {MatInputModule} from '@angular/material/input';
     MatFormFieldModule,
     MatInputModule,
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
   ],
   exports: [
     MatToolbarModule,
@@ -40,8 +37,6 @@ import {MatInputModule} from '@angular/material/input';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
   ],
   providers: [],
 })
--- src/app/views/questions-answers/questions-answers.module.ts
+++ src/app/views/questions-answers/questions-answers.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from "./../../@shared/shared.module";
+import { SharedModule } from './../../@shared/shared.module';
 import { QuestionsAnswersRoutingModule } from './questions-answers-routing.module';
 import { QuestionAnswerComponent } from './question-answer/question-answer.component';
 import { QuestionCardComponent } from './../../@shared/question-card/question-card.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [QuestionAnswerComponent, QuestionCardComponent],
   imports: [
     SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     QuestionsAnswersRoutingModule
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class QuestionsAnswersModule { }
